be2e83ed145b257a759faa5454e5069bef2bdc22
Akshat Tandon <akshattandon4@gmail.com>
2016-08-03 01:01:09 +0530

    initial commit

---
+++ Assets/BallMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    public int ballVelocity;
+    public GameObject brick;
+    bool isMoving;
+    
+    void Start() {
+        isMoving = true;
+        brick.GetComponent<Collider>();
+        gameObject.GetComponent<Collider>();
+    }
+    void FixedUpdate()
+    {   
+       
+    }
+    void Update() {
+       
+    }
+    void collision() {
+        if (isMoving)
+            transform.Translate(Vector3.up * Time.deltaTime * ballVelocity);
+            Vector3 fwd = gameObject.transform.TransformVector(0,gameObject.transform.position.y,0);
+            Vector3 dist = fwd + brick.transform.TransformVector(0,gameObject.transform.position.y,0);
+            Debug.Log(dist.magnitude);
+           if (dist.magnitude ==0){
+                isMoving = false;
+                ballVelocity = 0;
+           }
+          collision();
+       }
+           
+    
+    
+    }
---
+++ Assets/movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+    public bool updateOn = true;
+    public int vel;
+	// Use this for initialization
+    void Start(){
+        StartCoroutine(MyUpdate());
+ }
+
+    IEnumerator MyUpdate()
+    {
+        float timer = 0;
+       double timing = 0.64f / vel;
+        double time = 5f;
+        while (timer < time)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * vel);
+            timer += Time.deltaTime;
+            if (timer == timing)
+                vel = 0;
+            yield return null;
+        }
+        // Here do anything that needs be done after 5s
+    }
+}
